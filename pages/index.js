53f4efc88fe629cdf23db9347ce84369ff9bd67b
javacop01 <javacop@gmail.com>
2024-07-11 18:44:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,10 +56,10 @@ export default function Home() {
                   <h2>Contact Us</h2>
                   <form id="contactForm">
                       <label for="name">Name:</label>
-                      <input type="text" id="name" name="name" required>
+                      <input type="text" id="name" name="name" required></input>
       
                       <label for="email">Email:</label>
-                      <input type="email" id="email" name="email" required>
+                      <input type="email" id="email" name="email" required></input>
       
                       <label for="message">Message:</label>
                       <textarea id="message" name="message" required></textarea>
